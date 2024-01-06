75eb193fce9bcfce875a30128a29c0e780c06a27
SiriusTimeStop <jackychoikalok@gmail.com>
2024-01-06 21:23:51 +0800

    Create new post function

---
+++ SmartPaint/SmartPaint/Model/Post.swift
@@ -0,0 +1,36 @@
+//
+//  Post.swift
+//  SmartPaint
+//
+//  Created by jackychoi on 4/1/2024.
+//
+
+import SwiftUI
+import FirebaseFirestoreSwift
+
+//MARK: post model
+struct Post: Identifiable,Codable{
+    @DocumentID var id: String?
+    var text: String
+    var imageURL: URL?
+    var imageReferenceID: String = ""
+    var publishedDate: Date = Date()
+    var likedIDs: [String] = []
+    var dislikedIDs: [String] = []
+    var userName: String
+    var userID: String
+    var userProfileURL: URL
+    
+    enum CodingKeys: CodingKey{
+        case id
+        case text
+        case imageURL
+        case imageReferenceID
+        case publishedDate
+        case likedIDs
+        case dislikedIDs
+        case userName
+        case userID
+        case userProfileURL
+    }
+}
---
+++ SmartPaint/SmartPaint/View/MainView/CreateNewPost.swift
@@ -0,0 +1,20 @@
+//
+//  CreateNewPost.swift
+//  SmartPaint
+//
+//  Created by jackychoi on 4/1/2024.
+//
+
+import SwiftUI
+
+struct CreateNewPost: View {
+    
+    var onPost: () -> ()
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateNewPost()
+}
