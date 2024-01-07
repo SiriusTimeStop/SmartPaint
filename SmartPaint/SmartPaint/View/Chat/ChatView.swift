d5e406ba47b75920f44be7a503dd36f3ca826a13
SiriusTimeStop <jackychoikalok@gmail.com>
2024-01-08 00:13:07 +0800

    AI chatbot function

---
+++ SmartPaint/SmartPaint/View/Chat/ChatView.swift
@@ -0,0 +1,19 @@
+//
+//  ChatView.swift
+//  SmartPaint
+//
+//  Created by jackychoi on 8/1/2024.
+//
+
+import SwiftUI
+import GoogleGenerativeAI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
