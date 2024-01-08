a59a9533e3c5c15d36e031e710e7143993cb500a
SiriusTimeStop <jackychoikalok@gmail.com>
2024-01-08 16:40:07 +0800

    Set up Gemini AI api key

--- SmartPaint/SmartPaint/View/Chat/ChatView.swift
+++ SmartPaint/SmartPaint/View/Chat/ChatView.swift
@@ -9,8 +9,62 @@ import SwiftUI
 import GoogleGenerativeAI
 
 struct ChatView: View {
+    let model = GenerativeModel(name: "gemini-pro", apiKey: "")
+    @State var textInput = ""
+    @State var aiResponse = "Hello"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("LouvreImage")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200)
+            
+            //MARK: AI resposne
+            ScrollView{
+                Text(aiResponse)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+            }
+            
+            HStack{
+                TextField("Enter a message", text: $textInput)
+                    .textFieldStyle(.roundedBorder)
+                    .foregroundStyle(.black)
+                Button(action:{
+                    
+                },label: {
+                    Image(systemName: "paperplane.fill")
+                })
+            }
+        }
+        .foregroundStyle(.white)
+        .padding()
+        .background{
+            ZStack{
+                Color.black
+            }
+            .ignoresSafeArea()
+        }
+    }
+    
+    //MARK: fetch response
+    func sendMessage(){
+        aiResponse = ""
+        Task{
+            do{
+                let response = try await model.generateContent(textInput)
+                
+                guard let text = response.text else{
+                    textInput = "Sorry, I could not process that. \nPlease try again"
+                    return
+                }
+                
+                textInput = ""
+                aiResponse = text
+            }catch{
+                aiResponse = "Something went wrong \n\(error.localizedDescription)"
+            }
+        }
     }
 }
 
